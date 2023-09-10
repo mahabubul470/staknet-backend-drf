e14b6455c7699ffc2db82e97b8af71bd3f9c9b52
Mahabubul Hasan <mahabubul470@gmail.com>
2023-09-11 05:24:39 +0600

    Update endpoints

--- staknet/urls.py
+++ staknet/urls.py
@@ -1,12 +1,7 @@
 
 from django.urls import path
-# from core import views
+from django.urls import path, include
 
 urlpatterns = [
-    # path('profiles/', views.ProfileListCreateView.as_view(),
-    #      name='profile-list-create'),
-    # path('profiles/<str:pk>/', views.ProfileDetailView.as_view(),
-    #      name='profile-detail'),
-    # path('posts/', views.PostListCreateView.as_view(), name='post-list-create'),
-    # path('posts/<str:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('api/v1/', include('core.urls')),
 ]
